48a8eb636be4e35f24d4bc001b2b3fde40b5bcaa
Niall Nulty <nnulty97@gmail.com>
2020-10-29 16:18:45 +0000

    can access line renderer and assign points to another line renderer

---
+++ LineRendererManipulation/Assets/Scripts/LineManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineManager : MonoBehaviour
+{
+    //line renderers
+    [SerializeField] private LineRenderer line;
+    [SerializeField] private LineRenderer duplicateLine;
+
+    void Start()
+    {
+        //Get array of line renderer positions
+        Vector3[] linePositionsArray = new Vector3[line.positionCount];
+        line.GetPositions(linePositionsArray);
+
+        duplicateLine.positionCount = line.positionCount;
+
+        //set duplicate line positions based off array of positions
+        for (int i = 0; i < duplicateLine.positionCount; i++)
+        {
+            duplicateLine.SetPosition(i, linePositionsArray[i]);
+        }
+    }
+}
